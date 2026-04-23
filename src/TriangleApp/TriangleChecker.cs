61da8f546ef967f10967496e7f09b89cbeab379f
tregula124 <tomeeek124@onet.pl>
2026-04-23 20:35:27 +0200

    feature: create TriangleChecker class

---
+++ src/TriangleApp/TriangleChecker.cs
@@ -0,0 +1,16 @@
+﻿namespace TriangleApp;
+
+public class TriangleChecker
+{
+    public bool IsTriangle(int a, int b, int c)
+    {
+        // Check if side lengths are positive numbers
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            throw new ArgumentException("Side lengths are not positive numbers.");
+        }
+
+        // Check for triangle inequality
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+}
